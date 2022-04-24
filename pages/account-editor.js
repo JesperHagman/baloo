fb2647fa864582c66f4919674394a9fbe918eba0
Lovisa Eriksson <lovisa@ahna.se>
2022-04-24 13:19:36 +0200

    styling v1 done

--- pages/account-editor.js
+++ pages/account-editor.js
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import styles from '../styles/account-editor.module.css'
 
 export default function Editor () {
     const [disabled, setDisabled] = useState('disabled')
@@ -18,27 +19,29 @@ export default function Editor () {
         e.preventDefault()
     }
     return (
-        <div>
-        <form onSubmit={handleSubmit}>
-           <label htmlFor="email">Mail:</label>
-           <input type="email" name="email" disabled={disabled}/>
+        <div className={styles.wrapper}>
+            <h1>Edit your account</h1>
+            <form onSubmit={handleSubmit} className={styles.form}>
+                <label htmlFor="email">Mail:</label>
+                <input className={styles.input} type="email" name="email" disabled={disabled}/>
 
-           <label htmlFor="phone">Phone:</label>
-           <input type="text" name="phone" id="phone" disabled={disabled}/>
-           
-           <label htmlFor="fName">Firstname:</label>
-           <input type="text" name="fName" id="fName" disabled={disabled}/>
+                <label htmlFor="phone">Phone:</label>
+                <input className={styles.input} type="text" name="phone" id="phone" disabled={disabled}/>
+                
+                <label htmlFor="fName">Firstname:</label>
+                <input className={styles.input} type="text" name="fName" id="fName" disabled={disabled}/>
 
-           <label htmlFor="lName">Lastname:</label>
-           <input type="text" name="lName" id="lName" disabled={disabled}/>
+                <label htmlFor="lName">Lastname:</label>
+                <input className={styles.input} type="text" name="lName" id="lName" disabled={disabled}/>
 
-           <label htmlFor="bday">Birthday:</label>
-           <input type="date" name="bday" id="bday" disabled={disabled}/>
-           
-           <button onClick={() => handleClick(disabled)}>{btnName}</button>
+                <label htmlFor="bday">Birthday:</label>
+                <input className={styles.input} type="date" name="bday" id="bday" disabled={disabled}/>
 
-            <button>Delete my account</button>
-        </form>
+                <div className={styles.btn_wrapper}>
+                    <button className={styles.btn} onClick={() => handleClick(disabled)}>{btnName}</button>
+                    <button className={styles.btn}>Delete my account</button>
+                </div>
+            </form>
         </div>
     )
     
